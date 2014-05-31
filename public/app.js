6aac02d18d0026c5e40e57e6bf48d17bcdbd7282
Sam Stites <stites.sr@gmail.com>
2014-05-31 14:37:17 -0700

    update - moved filetypes into rootscope, added stateprovider

--- public/app.js
+++ public/app.js
@@ -1,2 +1,34 @@
-angular.module('pd', [])
+angular.module('pd', ['ui-router'])
+.config(function($stateProvider){
+  $stateProvider
+    .when('upload', {
+      url: '',
+      controller: 'upload',
+      template: '',
+    })
+    .when('download', {
+      url: 'download',
+      controller: 'download',
+      template: '',
+    })
+})
+.run(function($rootScope) {
+  $rootScope.formats = [
+    "XHTML", "HTML5", "HTML slideshows with Slidy",
+    "reveal.js", "Slideous", "S5", "DZSlides",
+    "Microsoft Word docx", "OpenOffice/LibreOffice ODT",
+    "OpenDocument XML", "EPUB v2", "EPUB v3",
+    "FictionBook2", "DocBook", "GNU TexInfo",
+    "Groff man pages", "Haddock markup", "InDesign ICML",
+    "OPML", "LaTeX", "ConTeXt", "LaTeX Beamer slides",
+    "via LaTeX", "Markdown", "reStructuredText",
+    "AsciiDoc", "MediaWiki markup", "Emacs Org-Mode",
+    "Textile", "Can be written in lua",
+  ]
+})
+.controller('upload', function(){
 
+})
+.controller('download', function(){
+
+})
