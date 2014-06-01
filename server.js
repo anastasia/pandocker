153df5965651f4c5007a2a6b47c1ea2fa4a3a648
anastasia <anastasia.aizman@gmail.com>
2014-05-31 18:44:23 -0700

    add - writing files and gzipping

--- server.js
+++ server.js
@@ -1,20 +1,74 @@
 var express         = require("express");
 var app             = express();
-var bodyParser      = require('body-parser');
-var errorHandler    = require('errorhandler');
-var methodOverride  = require('method-override');
 var port            = 3000;
-// var port            = parseInt(process.env.PORT, 10) || 4567;
-app.get("/", function (req, res) {
-  res.redirect("/index.html");
-});
 
-app.use(methodOverride());
-app.use(bodyParser());
+var jandoc   = require('jandoc');
+var fs       = require('fs');
+var targz    = require('tar.gz');
+var mkdirp   = require('mkdirp');
+var rmdir    = require('rimraf');
+
+app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
-app.use(errorHandler({
-  // dumpExceptions: true,
-  // showStack: true
-}));
 
 app.listen(port);
+
+var input  = __dirname + '/temp/input';
+var output = __dirname + '/temp/output';
+
+mkdirp(input, function (err) {
+  if (err) console.error(err)
+  else console.log('input!')
+});
+
+mkdirp(output, function (err) {
+  if (err) console.error(err)
+  else console.log('output!')
+});
+
+app.post('/upload', function(req, res) {
+
+  var filePath     = req.files.files.path;
+  var fileName     = req.files.files.name.split('.')[0];
+  var extensions   = req.body.data;
+
+  fs.readFile(filePath, function(err, data){
+    if(err){
+       throw err
+    } else {
+      fileToSendBack = processFile(filePath, data, extensions, fileName);
+    }
+  });
+  res.send('oook then')
+});
+
+var processFile = function(path, data, extensions, name) {
+  fs.writeFile(path, data, function(err) {
+    if(err) {
+      console.log(err)
+    } else {
+      extensions = JSON.parse(extensions)
+      for(var i = 0; i < extensions.length; i++) {
+        jandoc.cmd('-d '+path+ ' -o ' + output +' --write '+extensions[i]); // write each file in output directory
+      }
+
+      var compress = new targz().compress(
+                                  output, name+'.tar.gz', function(err){
+                                  if(err) throw err;
+                                  return name+'.tar.gz';
+                                });
+    }
+  });
+  deleteDirectories();
+};
+
+var deleteDirectories = function() {
+  console.log('WHOA THERE')
+
+  rmdir(__dirname+'/temp', function(err) {
+    if(err) throw err
+    else {
+      console.log('temp dir removed!');
+    }
+  });
+}
